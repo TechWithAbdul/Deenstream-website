767624d742e4190ba63839e11e5288c97f8814f3
TechWithAbdul <ar5431980@gmail.com>
2026-07-10 17:25:42 +0500

    Fix import case for AIChat component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Quran from './pages/Quran'
 import Hadith from './pages/Hadith'
 import Prayers from './pages/Prayers'
 import Duas from './pages/Duas'
-import AIChat from './pages/AIChat'
+import AIChat from './pages/AiChat'
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 
 // 404 view
